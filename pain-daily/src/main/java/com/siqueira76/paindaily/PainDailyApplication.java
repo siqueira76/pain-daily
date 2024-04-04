602dca1a8667a5f65e825a7edc15438ce45189bb
siqueira76 <josecarlos.siqueira76@gmail.com>
2024-04-04 12:55:29 -0300

    first commit

---
+++ pain-daily/src/main/java/com/siqueira76/paindaily/PainDailyApplication.java
@@ -0,0 +1,13 @@
+package com.siqueira76.paindaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainDailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainDailyApplication.class, args);
+	}
+
+}
---
+++ pain-daily/src/test/java/com/siqueira76/paindaily/PainDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siqueira76.paindaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainDailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
